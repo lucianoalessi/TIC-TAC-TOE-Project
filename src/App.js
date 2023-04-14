99f970bb335ab46f67fbb332f2a44b0383db389e
lucianoalessi9 <lucianoalessi9@hotmail.com>
2023-04-13 21:13:13 -0300

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,30 @@
 import React from 'react';
-export default function Square() {
-  return <button className="square">X</button>;
+
+function Square({value}){
+  return <button className="square">{value}</button>
+
+}
+
+export default function Board() {
+  return (
+
+<React.Fragment>
+  <div className="board-row">
+  <Square />
+  <Square />
+  <Square />
+  </div>
+  <div className="board-row">
+  <Square />
+  <Square />
+  <Square />
+  </div>
+  <div className="board-row">
+  <Square />
+  <Square />
+  <Square />
+  </div>
+  </React.Fragment>
+
+  );
 }
