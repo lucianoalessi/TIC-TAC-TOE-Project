9c754edf195f380502166a71780d6a3674f88dd4
lucianoalessi9 <lucianoalessi9@hotmail.com>
2023-04-14 23:39:33 -0300

    declarando un ganador

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
 import React from 'react';
 import {useState} from "react";
 
-function Square({value}){
 
-  return <button className="square">{value}</button>
+
+
+function Square({value , onSquareClick}){
+  return <button className="square" onClick={onSquareClick} >{value}</button>
 }
+
+
 
 export default function Board() {
 
+
+  const [xIsNext , setXIsNext] =useState(true)
   const [squares , setSquares] = useState(Array(9).fill(null))
+  
+  function handleClick(i){
+    if(squares(i)){        //aca lo que hacemos es comprobar si el cuadrado ya esta lleno.Como un string representa un "TRUE", si esta lleno con una X o una O, genera un return y no actualiza el estado. si hay un null seria un false, por lo tanto no ejecuta esa porcion de codigo y sigue con el resto. 
+      return
+    }
+    const nextSquares = squares.slice()
+    if(xIsNext){
+      nextSquares[i] ="X"
+    }else{
+      nextSquares[i]="O"
+    }
+    setSquares(nextSquares)
+    setXIsNext(!xIsNext)
+  }
+
   return (
 
 <React.Fragment>
   <div className="board-row">
-  <Square value={squares[0]}/>
-  <Square value={squares[1]}/>
-  <Square value={squares[2]}/>
+  <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+  <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+  <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
   </div>
   <div className="board-row">
-  <Square value={squares[3]} />
-  <Square value={squares[4]}/>
-  <Square value={squares[5]}/>
+  <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+  <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+  <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
   </div>
   <div className="board-row">
-  <Square value={squares[6]}/>
-  <Square value={squares[7]}/>
-  <Square value={squares[8]}/>
+  <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+  <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+  <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
   </div>
   </React.Fragment>
 
